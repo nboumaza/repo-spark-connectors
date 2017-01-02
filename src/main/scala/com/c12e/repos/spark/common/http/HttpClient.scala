7f00e585eee751ebe3710a0d6826f816e9711598
Nadia Boumaza <nboumaza@cognitivescale.com>
2017-01-02 00:24:46 -0600

    baseline http client  impl

--- src/main/scala/com/c12e/repos/spark/common/http/HttpClient.scala
+++ src/main/scala/com/c12e/repos/spark/common/http/HttpClient.scala
@@ -2,10 +2,10 @@ package com.c12e.repos.spark.common.http
 
 
 import com.c12e.extn.http4s.argonaut.NaiveArgonautCodec._
-import com.c12e.extn.http4s.client.{Client, ClientFault}
+import com.c12e.extn.http4s.client._
 import org.http4s.Uri.{Authority, RegName}
 import org.http4s._
-import org.http4s.client.blaze.{BlazeClientConfig, PooledHttp1Client}
+import org.http4s.client.blaze.{BlazeClientConfig, SimpleHttp1Client}
 import org.http4s.util.CaseInsensitiveString
 
 import scala.concurrent.duration._
@@ -13,75 +13,75 @@ import scalaz.{-\/, \/, \/-}
 
 object HttpClient {
 
-  type MaybeRepo = ClientFault \/ Repository
+  type MaybeRepo = \/[ClientFault, Repository]
 
   val API_Key: String = "X-CogScale-Key"
-  val REPO_SERVICE_HOST= "api.foundation.insights.ai"
+  val REPO_SERVICE_HOST = "api.foundation.insights.ai"
   val REPO_SERVICE_PORT = 80
-  val REPO_ENDPPOINT: String = "http://api.foundation.insights.ai/v1/repository/"
+  val REPO_ENDPOINT: String = "http://api.foundation.insights.ai/v1/repository/"
   val CON_INFO: String = "connection"
   val TIMEOUT_SECONDS: Int = 30
 
   val config_ = BlazeClientConfig.defaultConfig.copy(requestTimeout = TIMEOUT_SECONDS.second)
-  val client_ = Client.backedBy(PooledHttp1Client(config = config_))
+  val client_ = Client.backedBy(SimpleHttp1Client(config_))
 
-  /**
-    *
-    */
-  def shutdown: Unit = client_.shutdown
+
+  def shutdown() = client_.shutdown.unsafePerformSync
+
 
   /**
     * Fetches a repository connection details by api key and repo id
     *
     * @param repoId repository Id
     * @param apiKey api key
-    * @return  repository info
+    * @return optional Repository if successful fetch None otherwise
     */
-  def getRepoInfo(repoId: String, apiKey: String): MaybeRepo = {
+  def fetchRepoInfo(repoId: String, apiKey: String): Option[Repository] = {
 
-    val uri_ = Uri(path = s"${REPO_ENDPPOINT}${repoId}/${CON_INFO}",
-      authority = Some(Authority(host = RegName(REPO_SERVICE_HOST),
-        port = Some(REPO_SERVICE_PORT) )))
+    def getRepoInfo(repoId: String, apiKey: String):  Either[ClientFault, Repository] = {
 
-    val request = Request(
-      method = Method.GET,
-      uri = uri_,
-      headers = Headers(Header.Raw(CaseInsensitiveString(API_Key), apiKey))
-    )
+      val uri_ = Uri(path = s"$REPO_ENDPOINT$repoId/$CON_INFO",
+        authority = Some(Authority(host = RegName(REPO_SERVICE_HOST),
+          port = Some(REPO_SERVICE_PORT))))
 
-    val response = client_
-      .ask(request)
-      .decode[Repository]
-      .run
-      .unsafePerformSync
+      val request = Request(
+        method = Method.GET,
+        uri = uri_,
+        headers = Headers(Header.Raw(CaseInsensitiveString(API_Key), apiKey))
+      )
 
-    response match {
-      case \/-(repository) => println("Goooooooddddddd")
-      case -\/(error) =>  println("Ouch!")
+      client_
+        .ask(request)
+        .decode[Repository]
+        .run
+        .unsafePerformSync match {
+        case \/-(repo) => shutdown(); Right(repo)
+        case -\/(cf)   => shutdown(); Left(cf)
+      }
 
     }
-    response
+
+    getRepoInfo(repoId, apiKey) match {
+      case Right(repo) =>  Some(repo)
+      case Left(cf) => None
+    }
 
 
   }
 
 
-  /*private def handleResponse[A](response: ClientFault \/ A) = {
-    response.fold(notFoundToNone,a => success(some(a)))
-  }*/
+  //TODO remove test
+    def main(args: Array[String]): Unit = {
 
-  def main(args: Array[String]): Unit = {
+      val repo = fetchRepoInfo("582dc1fe4a8a2e0011880682", "4346c61a3d0d476caeb25b412cad4e0c")
 
-    val maybeRepo = getRepoInfo("582dc1fe4a8a2e0011880682",  "4346c61a3d0d476caeb25b412cad4e0c")
-
-    shutdown
-
-    println(maybeRepo.getOrElse("bla"))
-
-
+      if (repo.isDefined)
+        println(s"\n url = ${repo.get.server.host}:${repo.get.server.port}" +
+                s"\n database = ${repo.get.database}")
+      else
+        println(">>See no need to pursue....TODO ")
+    }
 
 
-
-  }
 
 }
